cb25e7dc018f4942fb73e0236245bb97fbf76705
Rich Lysakowski <rich.lysakowski@gmail.com>
2023-06-10 18:07:06 -0400

    Fix minor dirty code, such as f-strings, etc.

--- Fake_Data_Generator_App/fake_data_generator_app.py
+++ Fake_Data_Generator_App/fake_data_generator_app.py
@@ -1,3 +1,5 @@
+# Updated by: Rich Lysakowski
+# Updated On: 2023-06-10
 
 # Core Pkgs
 import streamlit as st 
@@ -19,21 +21,21 @@ def make_downloadable_df(data):
     csvfile = data.to_csv(index=False)
     b64 = base64.b64encode(csvfile.encode()).decode()  # B64 encoding
     st.markdown("### ** Download CSV File ** ")
-    new_filename = "fake_dataset_{}.csv".format(timestr)
+    new_filename = f"fake_dataset_{timestr}.csv"
     href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!</a>'
     st.markdown(href, unsafe_allow_html=True)
 
 # Fxn to Download Into A Format
-def make_downloadable_df_format(data,format_type="csv"):
-	if format_type == "csv":
-		datafile = data.to_csv(index=False)
-	elif format_type == "json":
-		datafile = data.to_json()
-	b64 = base64.b64encode(datafile.encode()).decode()  # B64 encoding
-	st.markdown("### ** Download File  📩 ** ")
-	new_filename = "fake_dataset_{}.{}".format(timestr,format_type)
-	href = f'<a href="data:file/{format_type};base64,{b64}" download="{new_filename}">Click Here!</a>'
-	st.markdown(href, unsafe_allow_html=True)
+def make_downloadable_df_format(data, format_type="csv"):
+    if format_type == "csv":
+    	datafile = data.to_csv(index=False)
+    elif format_type == "json":
+    	datafile = data.to_json()
+    b64 = base64.b64encode(datafile.encode()).decode()  # B64 encoding
+    st.markdown("### ** Download File  📩 ** ")
+    new_filename = f"fake_dataset_{timestr}.{format_type}"
+    href = f'<a href="data:file/{format_type};base64,{b64}" download="{new_filename}">Click Here!</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
 
 # Generate A Simple Profile
@@ -95,20 +97,25 @@ def main():
 
 		df = generate_locale_profile(number_to_gen,locale)
 		st.dataframe(df)
-		with st.beta_expander("📩: Download"):
+		with st.expander("📩: Download"):
 			make_downloadable_df_format(df,dataformat)
 
 	elif choice == "Customize":
 		st.subheader("Customize Your Fields")
 		# Locale Providers For Faker Class
 		localized_providers = ["ar_AA", "ar_EG", "ar_JO", "ar_PS", "ar_SA", "bg_BG", "bs_BA", "cs_CZ", "de", "de_AT", "de_CH", "de_DE", "dk_DK", "el_CY", "el_GR", "en", "en_AU", "en_CA", "en_GB", "en_IE", "en_IN", "en_NZ", "en_PH", "en_TH", "en_US", "es", "es_CA", "es_ES", "es_MX", "et_EE", "fa_IR", "fi_FI", "fil_PH", "fr_CA", "fr_CH", "fr_FR", "fr_QC", "he_IL", "hi_IN", "hr_HR", "hu_HU", "hy_AM", "id_ID", "it_CH", "it_IT", "ja_JP", "ka_GE", "ko_KR", "la", "lb_LU", "lt_LT", "lv_LV", "mt_MT", "ne_NP", "nl_BE", "nl_NL", "no_NO", "or_IN", "pl_PL", "pt_BR", "pt_PT", "ro_RO", "ru_RU", "sk_SK", "sl_SI", "sv_SE", "ta_IN", "th", "th_TH", "tl_PH", "tr_TR", "tw_GH", "uk_UA", "zh_CN", "zh_TW"]
-		locale = st.sidebar.multiselect("Select Locale",localized_providers,default="en_US")
+		locale = st.sidebar.multiselect("Select Locale", localized_providers, default="en_US")
 		
-		profile_options_list = ['username', 'name', 'sex' , 'address', 'mail' , 'birthdate''job', 'company', 'ssn', 'residence', 'current_location', 'blood_group', 'website'] 
-		profile_fields = st.sidebar.multiselect("Fields",profile_options_list,default='username')
+		# Profile Fields
+		# New Fields Can Be Added Here
+		#profile_options_list = ["FirstName", "LastName", "TechSupportRole", "Region",  "Country", "Manager", "BusinessUnit"]		
+  		#profile_tech_support_app = ["FirstName", "LastName", "TechSupportRole", "Region",  "Country", "Manager", "BusinessUnit"]
 
-		number_to_gen = st.sidebar.number_input("Number",10,10000)
-		dataformat = st.sidebar.selectbox("Save Data As",["csv","json"])
+		profile_options_list = ['username', 'name', 'sex' , 'address', 'mail' , 'birthdate''job', 'company', 'ssn', 'residence', 'current_location', 'blood_group', 'website'] 
+		profile_fields = st.sidebar.multiselect("Fields", profile_options_list, default='username')
+
+		number_to_gen = st.sidebar.number_input("Number", 10,10000)
+		dataformat = st.sidebar.selectbox("Save Data As", ["csv","json"])
 
 		# Initialize Faker Class
 		custom_fake = Faker(locale)
@@ -119,12 +126,11 @@ def main():
 		st.dataframe(df)
 
 		# View as JSON
-		with st.beta_expander("🔍: View JSON "):
+		with st.expander("🔍: View JSON "):
 			st.json(data)
 
-		with st.beta_expander("📩: Download"):
+		with st.expander("📩: Download"):
 			make_downloadable_df_format(df,dataformat)
-		
 
 	else:
 		st.subheader("About")
---
+++ Fake_Data_Generator_App/faker_testing_RSL_WIP.py
@@ -0,0 +1,13 @@
+# Core Pkgs
+import streamlit as st 
+import streamlit.components.v1 as stc 
+
+# Data Pkgs
+import pandas as pd 
+from faker import Faker
+
+
+# Utils
+import base64
+import time 
+timestr = time.strftime("%Y%m%d-%H%M%S")
